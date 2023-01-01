bdfdff6809a25f75cb1facc274166ee1ce115ad1
Marcin Kern <silonpl@gmail.com>
2023-01-01 12:47:49 +0100

    Refactoring out common part of tests in day 07

--- test/Day07Test/SolutionTests.cs
+++ test/Day07Test/SolutionTests.cs
@@ -4,10 +4,7 @@ namespace Day07Test;
 
 public class SolutionTests
 {
-    [Fact]
-    public void TotalSumOfDirectoriesWithDuplicates()
-    {
-        const string input = @"$ cd /
+    private const string Input = @"$ cd /
 $ ls
 dir a
 14848514 b.txt
@@ -30,37 +27,18 @@ $ ls
 8033020 d.log
 5626152 d.ext
 7214296 k";
-        var result = Solution.SumOfDirectoriesSizes(input);
+    
+    [Fact]
+    public void TotalSumOfDirectoriesWithDuplicates()
+    {
+        var result = Solution.SumOfDirectoriesSizes(Input);
         Assert.Equal(95_437, result);
     }
     
     [Fact]
     public void SizeOfSmallestDeletedFolderToRunUpdate()
     {
-        const string input = @"$ cd /
-$ ls
-dir a
-14848514 b.txt
-8504156 c.dat
-dir d
-$ cd a
-$ ls
-dir e
-29116 f
-2557 g
-62596 h.lst
-$ cd e
-$ ls
-584 i
-$ cd ..
-$ cd ..
-$ cd d
-$ ls
-4060174 j
-8033020 d.log
-5626152 d.ext
-7214296 k";
-        var result = Solution.SizeOfSmallestDeletedFolderToRunUpdate(input);
+        var result = Solution.SizeOfSmallestDeletedFolderToRunUpdate(Input);
         Assert.Equal(24_933_642, result);
     }
 }
