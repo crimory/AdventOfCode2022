65708fcfd606ea60f43813e9e298b60fde3bb00e
Marcin Kern <silonpl@gmail.com>
2023-01-07 12:20:38 +0100

    Reorder tests for day 08

--- test/Day08Test/SolutionTests.cs
+++ test/Day08Test/SolutionTests.cs
@@ -17,43 +17,7 @@ public class SolutionTests
         
         Assert.Equal(21, result);
     }
-    
-    [Fact]
-    public void HighestPossibleScenicScore_Correct()
-    {
-        var result = Solution.HighestPossibleScenicScore(Input);
-        
-        Assert.Equal(8, result);
-    }
-    
-    [Theory]
-    [InlineData(1, 2, 4)]
-    [InlineData(3, 2, 8)]
-    public void GetScenicScoreTests(int rowIndex, int treeIndex, int expectedScenicScore)
-    {
-        var trees = Solution.GetTrees(Input);
-        var result = Solution.GetScenicScore(trees, rowIndex, treeIndex);
-        
-        Assert.Equal(expectedScenicScore, result);
-    }
-    
-    [Theory]
-    [InlineData(1, 2, Solution.LookingDirection.Top, 1)]
-    [InlineData(1, 2, Solution.LookingDirection.Left, 1)]
-    [InlineData(1, 2, Solution.LookingDirection.Right, 2)]
-    [InlineData(1, 2, Solution.LookingDirection.Bottom, 2)]
-    [InlineData(3, 2, Solution.LookingDirection.Top, 2)]
-    [InlineData(3, 2, Solution.LookingDirection.Left, 2)]
-    [InlineData(3, 2, Solution.LookingDirection.Right, 2)]
-    [InlineData(3, 2, Solution.LookingDirection.Bottom, 1)]
-    public void GetDirectionalScenicSubScoreTests(int rowIndex, int treeIndex, Solution.LookingDirection direction, int expectedScenicScore)
-    {
-        var trees = Solution.GetTrees(Input);
-        var result = Solution.GetDirectionalScenicSubScore(trees, rowIndex, treeIndex, direction);
-        
-        Assert.Equal(expectedScenicScore, result);
-    }
-    
+
     [Theory]
     [InlineData(0, 1, 3, 3, true)]
     [InlineData(1, 1, 3, 3, false)]
@@ -100,5 +64,41 @@ public class SolutionTests
         };
         var result = Solution.IsVisibleByIndex(trees, 1, 1);
         Assert.Equal(expectedVisibility, result);
+    }
+    
+    [Fact]
+    public void HighestPossibleScenicScore_Correct()
+    {
+        var result = Solution.HighestPossibleScenicScore(Input);
+        
+        Assert.Equal(8, result);
+    }
+    
+    [Theory]
+    [InlineData(1, 2, 4)]
+    [InlineData(3, 2, 8)]
+    public void GetScenicScoreTests(int rowIndex, int treeIndex, int expectedScenicScore)
+    {
+        var trees = Solution.GetTrees(Input);
+        var result = Solution.GetScenicScore(trees, rowIndex, treeIndex);
+        
+        Assert.Equal(expectedScenicScore, result);
+    }
+    
+    [Theory]
+    [InlineData(1, 2, Solution.LookingDirection.Top, 1)]
+    [InlineData(1, 2, Solution.LookingDirection.Left, 1)]
+    [InlineData(1, 2, Solution.LookingDirection.Right, 2)]
+    [InlineData(1, 2, Solution.LookingDirection.Bottom, 2)]
+    [InlineData(3, 2, Solution.LookingDirection.Top, 2)]
+    [InlineData(3, 2, Solution.LookingDirection.Left, 2)]
+    [InlineData(3, 2, Solution.LookingDirection.Right, 2)]
+    [InlineData(3, 2, Solution.LookingDirection.Bottom, 1)]
+    public void GetDirectionalScenicSubScoreTests(int rowIndex, int treeIndex, Solution.LookingDirection direction, int expectedScenicScore)
+    {
+        var trees = Solution.GetTrees(Input);
+        var result = Solution.GetDirectionalScenicSubScore(trees, rowIndex, treeIndex, direction);
+        
+        Assert.Equal(expectedScenicScore, result);
     }
 }
