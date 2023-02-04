8c6c8d9af3d215f01689f22238afb70ed55e6f87
Marcin Kern <silonpl@gmail.com>
2023-02-04 15:23:10 +0100

    Day15 part 2 refactoring

--- rust/day15/src/day15.rs
+++ rust/day15/src/day15.rs
@@ -9,6 +9,22 @@ struct Sensor {
     x: i32,
     y: i32,
     closest_beacon: Beacon,
+    length_to_closest_beacon: i32,
+}
+impl Sensor {
+    fn new(sensor_x: i32, sensor_y: i32, beacon_x: i32, beacon_y: i32) -> Sensor {
+        Sensor {
+            x: sensor_x,
+            y: sensor_y,
+            closest_beacon: Beacon {
+                x: beacon_x,
+                y: beacon_y,
+            },
+            length_to_closest_beacon: get_length_between_points(
+                sensor_x, sensor_y, beacon_x, beacon_y,
+            ),
+        }
+    }
 }
 
 enum MapSizeDimension {
@@ -32,14 +48,12 @@ fn read_input_line(line: &str) -> Sensor {
         .flat_map(|s| s.skip(1))
         .map(|s| s.trim().parse::<i32>().unwrap())
         .collect::<Vec<i32>>();
-    Sensor {
-        x: raw_coordinates[0],
-        y: raw_coordinates[1],
-        closest_beacon: Beacon {
-            x: raw_coordinates[2],
-            y: raw_coordinates[3],
-        },
-    }
+    Sensor::new(
+        raw_coordinates[0],
+        raw_coordinates[1],
+        raw_coordinates[2],
+        raw_coordinates[3],
+    )
 }
 
 fn read_input(input: &str) -> Vec<Sensor> {
@@ -77,14 +91,14 @@ fn get_map_size(map: &[Sensor]) -> MapSize {
     }
 }
 
-fn get_length_to_point(x: i32, y: i32, sensor: &Sensor) -> i32 {
-    let x_diff = (sensor.x - x).abs();
-    let y_diff = (sensor.y - y).abs();
+fn get_length_between_points(x_1: i32, y_1: i32, x_2: i32, y_2: i32) -> i32 {
+    let x_diff = (x_2 - x_1).abs();
+    let y_diff = (y_2 - y_1).abs();
     x_diff + y_diff
 }
 
-fn get_length_to_closest_beacon(sensor: &Sensor) -> i32 {
-    get_length_to_point(sensor.closest_beacon.x, sensor.closest_beacon.y, sensor)
+fn get_length_to_point(x: i32, y: i32, sensor: &Sensor) -> i32 {
+    get_length_between_points(x, y, sensor.x, sensor.y)
 }
 
 fn is_position_a_beacon(x: i32, y: i32, map: &[Sensor]) -> bool {
@@ -99,8 +113,7 @@ fn is_position_a_beacon(x: i32, y: i32, map: &[Sensor]) -> bool {
 fn is_position_occupied(x: i32, y: i32, map: &[Sensor]) -> bool {
     for sensor in map {
         let length_to_point = get_length_to_point(x, y, sensor);
-        let length_to_closest_beacon = get_length_to_closest_beacon(sensor);
-        if length_to_point <= length_to_closest_beacon {
+        if length_to_point <= sensor.length_to_closest_beacon {
             return true;
         }
     }
@@ -117,95 +130,31 @@ pub fn get_occupied_positions_which_are_not_beacons(y: i32, input: &str) -> i32 
         .count() as i32
 }
 
-pub fn get_tuning_frequency_for_distress_beacon_v1(
-    search_min: i32,
-    search_max: i32,
-    input: &str,
-) -> i64 {
-    let map = read_input(input);
-    for x in search_min..=search_max {
-        for y in search_min..=search_max {
-            if is_position_occupied(x, y, &map) {
-                continue;
-            }
-            return (x as i64) * 4_000_000 + (y as i64);
+fn get_sorted_x_ranges_for_specific_y(y: i32, sensors: &[Sensor]) -> Vec<(i32, i32)> {
+    let mut ranges = vec![];
+    for sensor in sensors {
+        let length_left = sensor.length_to_closest_beacon - (y - sensor.y).abs();
+        if length_left < 0 {
+            continue;
         }
+        ranges.push((sensor.x - length_left, sensor.x + length_left))
     }
-    0
+    ranges.sort_by(|a, b| a.0.cmp(&b.0));
+    ranges
 }
 
-fn read_map_occupation(
-    search_min: i32,
-    search_max: i32,
-    map: &[Sensor]
-) -> Vec<Vec<bool>> {
-    let mut map_tests = vec![
-        vec![false; (search_max - search_min + 1) as usize];
-        (search_max - search_min + 1) as usize
-    ];
-    for sensor in map {
-        let length_to_closest_beacon = get_length_to_closest_beacon(sensor);
-        for y in (sensor.y-length_to_closest_beacon)..=(sensor.y+length_to_closest_beacon) {
-            let length_already_covered_by_y = (y - sensor.y).abs();
-            for x in (sensor.x-length_to_closest_beacon)..=(sensor.x+length_to_closest_beacon) {
-                if x<search_min || x>search_max || y<search_min || y>search_max {
-                    continue;
-                }
-                let length_to_be_covered_by_x = length_to_closest_beacon - length_already_covered_by_y;
-                if (x - sensor.x).abs() > length_to_be_covered_by_x {
-                    continue;
-                }
-
-                map_tests[(x as usize)][(y as usize)] = true;
-            }
-        }
-    }
-    map_tests
-}
-
-fn get_distress_beacon_position(map_occupation: Vec<Vec<bool>>) -> (i32, i32) {
-    for (x, row) in map_occupation.iter().enumerate() {
-        for (y, occupied) in row.iter().enumerate() {
-            if !occupied {
-                return (x as i32, y as i32);
-            }
-        }
-    }
-    (0, 0)
-}
-
-pub fn get_tuning_frequency_for_distress_beacon_v2(
-    search_min: i32,
-    search_max: i32,
-    input: &str,
-) -> i64 {
-    let map = read_input(input);
-    let map_occupation = read_map_occupation(search_min, search_max, &map);
-    let (x, y) = get_distress_beacon_position(map_occupation);
-    (x as i64) * 4_000_000 + (y as i64)
-}
-
-pub fn get_tuning_frequency_for_distress_beacon_v3(
+pub fn get_tuning_frequency_for_distress_beacon(
     search_min: i32,
     search_max: i32,
     input: &str,
 ) -> i64 {
     let sensors = read_input(input);
     for y in search_min..=search_max {
-        let mut ranges = vec![];
-        for sensor in &sensors {
-            let length_to_beacon = get_length_to_closest_beacon(sensor);
-            let length_left = length_to_beacon - (y - sensor.y).abs();
-            if length_left < 0 {
-                continue;
-            }
-            ranges.push((sensor.x - length_left, sensor.x + length_left))
-        }
-        ranges.sort_by(|a, b| a.0.cmp(&b.0));
+        let ranges = get_sorted_x_ranges_for_specific_y(y, &sensors);
         let mut max_x = search_min;
         for range in ranges {
             if range.0 > (max_x + 1) {
-                return ((max_x + 1) as i64) * 4_000_000 + (y as i64)
+                return ((max_x + 1) as i64) * 4_000_000 + (y as i64);
             }
             if range.1 > max_x {
                 max_x = range.1;
@@ -238,11 +187,7 @@ Sensor at x=20, y=1: closest beacon is at x=15, y=3";
 
     #[test]
     fn read_line_correctly() {
-        let expected = Sensor {
-            x: 2,
-            y: 18,
-            closest_beacon: Beacon { x: -2, y: 15 },
-        };
+        let expected = Sensor::new(2, 18, -2, 15);
         let result = read_input_line(INPUT_LINE);
         assert_eq!(result, expected);
     }
@@ -250,76 +195,20 @@ Sensor at x=20, y=1: closest beacon is at x=15, y=3";
     #[test]
     fn read_input_correctly() {
         let expected = vec![
-            Sensor {
-                x: 2,
-                y: 18,
-                closest_beacon: Beacon { x: -2, y: 15 },
-            },
-            Sensor {
-                x: 9,
-                y: 16,
-                closest_beacon: Beacon { x: 10, y: 16 },
-            },
-            Sensor {
-                x: 13,
-                y: 2,
-                closest_beacon: Beacon { x: 15, y: 3 },
-            },
-            Sensor {
-                x: 12,
-                y: 14,
-                closest_beacon: Beacon { x: 10, y: 16 },
-            },
-            Sensor {
-                x: 10,
-                y: 20,
-                closest_beacon: Beacon { x: 10, y: 16 },
-            },
-            Sensor {
-                x: 14,
-                y: 17,
-                closest_beacon: Beacon { x: 10, y: 16 },
-            },
-            Sensor {
-                x: 8,
-                y: 7,
-                closest_beacon: Beacon { x: 2, y: 10 },
-            },
-            Sensor {
-                x: 2,
-                y: 0,
-                closest_beacon: Beacon { x: 2, y: 10 },
-            },
-            Sensor {
-                x: 0,
-                y: 11,
-                closest_beacon: Beacon { x: 2, y: 10 },
-            },
-            Sensor {
-                x: 20,
-                y: 14,
-                closest_beacon: Beacon { x: 25, y: 17 },
-            },
-            Sensor {
-                x: 17,
-                y: 20,
-                closest_beacon: Beacon { x: 21, y: 22 },
-            },
-            Sensor {
-                x: 16,
-                y: 7,
-                closest_beacon: Beacon { x: 15, y: 3 },
-            },
-            Sensor {
-                x: 14,
-                y: 3,
-                closest_beacon: Beacon { x: 15, y: 3 },
-            },
-            Sensor {
-                x: 20,
-                y: 1,
-                closest_beacon: Beacon { x: 15, y: 3 },
-            },
+            Sensor::new(2, 18, -2, 15),
+            Sensor::new(9, 16, 10, 16),
+            Sensor::new(13, 2, 15, 3),
+            Sensor::new(12, 14, 10, 16),
+            Sensor::new(10, 20, 10, 16),
+            Sensor::new(14, 17, 10, 16),
+            Sensor::new(8, 7, 2, 10),
+            Sensor::new(2, 0, 2, 10),
+            Sensor::new(0, 11, 2, 10),
+            Sensor::new(20, 14, 25, 17),
+            Sensor::new(17, 20, 21, 22),
+            Sensor::new(16, 7, 15, 3),
+            Sensor::new(14, 3, 15, 3),
+            Sensor::new(20, 1, 15, 3),
         ];
         let result = read_input(INPUT);
         assert_eq!(result, expected);
@@ -340,24 +229,9 @@ Sensor at x=20, y=1: closest beacon is at x=15, y=3";
 
     #[test]
     fn get_length_to_point_01_correctly() {
-        let sensor = Sensor {
-            x: 1,
-            y: 2,
-            closest_beacon: Beacon { x: 0, y: 0 },
-        };
+        let sensor = Sensor::new(1, 2, 0, 0);
         let result = get_length_to_point(0, 0, &sensor);
         assert_eq!(3, result);
-    }
-
-    #[test]
-    fn get_length_to_closest_beacon_01_correctly() {
-        let sensor = Sensor {
-            x: 1,
-            y: 2,
-            closest_beacon: Beacon { x: -5, y: 0 },
-        };
-        let result = get_length_to_closest_beacon(&sensor);
-        assert_eq!(8, result);
     }
 
     #[test]
@@ -388,20 +262,8 @@ Sensor at x=20, y=1: closest beacon is at x=15, y=3";
     }
 
     #[test]
-    fn get_tuning_frequency_for_distress_beacon_v1_correctly() {
-        let result = get_tuning_frequency_for_distress_beacon_v1(0, 20, INPUT);
-        assert_eq!(56_000_011, result)
-    }
-
-    #[test]
-    fn get_tuning_frequency_for_distress_beacon_v2_correctly() {
-        let result = get_tuning_frequency_for_distress_beacon_v2(0, 20, INPUT);
-        assert_eq!(56_000_011, result)
-    }
-
-    #[test]
-    fn get_tuning_frequency_for_distress_beacon_v3_correctly() {
-        let result = get_tuning_frequency_for_distress_beacon_v3(0, 20, INPUT);
+    fn get_tuning_frequency_for_distress_beacon_correctly() {
+        let result = get_tuning_frequency_for_distress_beacon(0, 20, INPUT);
         assert_eq!(56_000_011, result)
     }
 }
--- rust/day15/src/main.rs
+++ rust/day15/src/main.rs
@@ -8,6 +8,6 @@ fn main() {
         day15::get_occupied_positions_which_are_not_beacons(y, input);
     println!("Number of positions on row {y}, that cannot contain a beacon: {number_of_occupied_positions}");
 
-    let tuning_frequency = day15::get_tuning_frequency_for_distress_beacon_v3(0, 4_000_000, input);
+    let tuning_frequency = day15::get_tuning_frequency_for_distress_beacon(0, 4_000_000, input);
     println!("Tuning frequency of distress beacon: {tuning_frequency}");
 }
