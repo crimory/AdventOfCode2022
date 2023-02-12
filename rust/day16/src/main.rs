f00d64d0c4ce77f148a26f010de0192d2174008a
Marcin Kern <silonpl@gmail.com>
2023-02-12 09:45:30 +0100

    Day16 ready for comparison

--- rust/day16/src/main.rs
+++ rust/day16/src/main.rs
@@ -1,12 +1,29 @@
 mod day16;
 mod day16_2;
 
+enum Version {
+    One,
+    Two,
+}
+
 fn main() {
+    let version = Version::Two;
     let input = include_str!("input.txt");
 
-    let pressure_released = day16_2::most_pressure_released(input);
-    println!("Pressure released: {pressure_released}");
+    match version {
+        Version::One => {
+            let pressure_released = day16::most_pressure_released(input);
+            println!("Pressure released v1: {pressure_released}");
 
-    let pressure_released = day16_2::most_pressure_released_with_two_actors(input);
-    println!("Pressure released for 2: {pressure_released}");
+            let pressure_released = day16::most_pressure_released_with_two_actors(input);
+            println!("Pressure released v1 for 2: {pressure_released}");
+        }
+        Version::Two => {
+            let pressure_released = day16_2::most_pressure_released(input);
+            println!("Pressure released v2: {pressure_released}");
+
+            let pressure_released = day16_2::most_pressure_released_with_two_actors(input);
+            println!("Pressure released v2 for 2: {pressure_released}");
+        }
+    }
 }
